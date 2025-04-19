a96a2de041ec0a15faafe0be2fd8777f318a2335
shravan kumar <shravan95741998@gmail.com>
2025-04-19 20:59:27 +0530

    archive😊

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+
+
+
+import React from 'react'
+
+function App() {
+  return (
+    <div></div>
+  )
+}
+
+export default App
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Apple from './Apple';
+// import Sample from './example/Sample';
+// import './sample.css'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Apple></Apple>
+//       <Sample></Sample>
+//       <h1>This is sampl css file</h1>
+//     </div>
+//   );
+// }
+
+// export default App;

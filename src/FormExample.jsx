e0dda7434611d49e98f8a388d003d41a7717515b
shravan kumar <shravan95741998@gmail.com>
2025-05-23 17:19:05 +0530

    controlled comp

--- src/FormExample.jsx
+++ src/FormExample.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 const FormExample = () => {
 
     const [userName, setUserName] = useState("warangal")
+    
+    const [newUserDetails,setNewUserdetails] = useState()
 
     // it is a anonimous function
     const getUserName = (event) => {
@@ -14,14 +16,18 @@ const FormExample = () => {
         // method body
     }
 
+    const userDetails=()=>{
+        setNewUserdetails(userName)
+    }  
+    
     return (
         <section className='formSection' >
-            <h2>Hello, {userName}</h2>
+            <h2>Hello,{newUserDetails}</h2>
             <div className='inputDiv'>
                 {/* <form onSubmit={userdetails}> */}
                     <input type="text" placeholder='Enter your Name' onChange={getUserName} />
                     <br></br>
-                    <button className='submitBtn' type='submit'>Submit</button>
+                    <button className='submitBtn' type='submit' onClick={userDetails}>Submit</button>
                 {/* </form> */}
             </div>
         </section>
